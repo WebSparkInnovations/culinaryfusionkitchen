ad9098bb919bba316e8e23bc9314fa6623f2d6e6
WSI <websparkinnovations@gmail.com>
2025-06-01 20:54:14 -0400

    email security

--- src/lib/sendMail.jsx
+++ src/lib/sendMail.jsx
@@ -6,8 +6,8 @@ const transporter = nodemailer.createTransport({
   port: 587,          
   secure: false,      
   auth: {
-    user: "cfkdummy@gmail.com", 
-    pass: "ofsklkujrmwpdizx",      
+    user: process.env.EMAIL_USER, 
+    pass: process.env.EMAIL_PASSWORD,      
   },
 });
 
